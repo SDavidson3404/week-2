010e8790a9bf4c5bb909ba874d0f991dd2654913
SDavidson3404 <bd1127702@student.francistuttle.edu>
2025-08-20 12:56:31 -0500

    C# Week 2
    Added work for Week 2 in C#

---
+++ C# Age classifier.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Please input your age");
+    var age = Console.ReadLine();
+if (int.TryParse(age, out int parsedAge))
+    {
+    if (parsedAge < 0)
+    {
+        Console.WriteLine("Age cannot be negative.");
+    }
+    else if (parsedAge <= 3)
+    {
+        Console.WriteLine("You are a toddler.");
+    }
+    else if (parsedAge <= 12)
+    {
+        Console.WriteLine("You are a child.");
+    }
+    else if (parsedAge <= 19)
+    {
+        Console.WriteLine("You are a teenager.");
+    }
+    else if (parsedAge <= 59)
+    {
+        Console.WriteLine("You are an adult.");
+    }
+    else
+    {
+        Console.WriteLine("You are a senior citizen.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid age.");
+}
---
+++ C# leap year.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Please enter your age: ");
+    var age = Console.ReadLine();
+if (int.TryParse(age, out int ageNumber))
+    {
+    if (ageNumber < 0)
+    {
+        Console.WriteLine("Age cannot be negative.");
+    }
+    else if (ageNumber <= 3)
+    {
+        Console.WriteLine("You are a toddler.");
+    }
+    else if (ageNumber >= 4 && ageNumber <= 12)
+    {
+        Console.WriteLine("You are a child.");
+    }
+    else if (ageNumber >= 20 && ageNumber <= 59)
+    {
+        Console.WriteLine($"You are an adult.");
+    else
+        {
+            Console.WriteLine("You are a Senior.");
+        }
+    }
+else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid year.");
+    }
